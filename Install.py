9dc90338f4f7e44d8115b05b6150f5fa21efe2e7
Z-BL4CX-H4T <123639062+Z-BL4CX-H4T@users.noreply.github.com>
2024-12-10 11:23:11 +0800

    Update Install.py

--- Install.py
+++ Install.py
@@ -3,7 +3,7 @@ from time import sleep
 
 def menu():
     system('clear')
-    syetem('pip install pyarmor')
+    system('pip install pyarmor')
     system('pyarmor obfuscate Universal-Checker.py')
     system('pyarmor obfuscate Universal-CheckerV2.py')
 menu()
