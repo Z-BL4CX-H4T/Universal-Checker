1d5b5c0df58d5d46bc2ff207173d20be964bad72
Z-BL4CX-H4T <123639062+Z-BL4CX-H4T@users.noreply.github.com>
2024-12-10 11:13:51 +0800

    Update and rename main.py to Install.py

--- Install.py
+++ Install.py
@@ -4,5 +4,6 @@ from time import sleep
 def menu():
     system('clear')
     syetem('pip install pyarmor')
-    system('pyarmor obfuscate your_script.py')
+    system('pyarmor obfuscate Universal-Checker.py')
+    system('pyarmor obfuscate Universal-CheckerV2.py')
 menu()
